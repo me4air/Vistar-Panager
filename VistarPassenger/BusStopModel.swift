9dd6c032c3cdd151d708a617fbe5c6639d0c01c0
Vsevolod Vtornikov <me4air@gmail.com>
2018-07-05 16:02:20 +0300

    Prepare fore Siesta!

---
+++ VistarPassenger/BusStopModel.swift
@@ -0,0 +1,26 @@
+//
+//  BusStopModel.swift
+//  VistarPassenger
+//
+//  Created by Всеволод on 05.07.2018.
+//  Copyright © 2018 me4air. All rights reserved.
+//
+
+import Foundation
+
+
+//Реализуем 2 Decodable структуры, чтобы swift сам распарсил из JSON
+
+struct busStopsResponce: Decodable {
+    var status: String?
+    var hash: Int?
+    var stops: Dictionary<String, BusStop>?
+}
+
+struct BusStop: Decodable {
+    var comment: String?
+    var id: Int?
+    var lat: Double?
+    var lon: Double?
+    var name: String?
+}
--- VistarPassenger/NetworckMangager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NetworckMangager.swift
-//  VistarPassenger
-//
-//  Created by Всеволод on 05.07.2018.
-//  Copyright © 2018 me4air. All rights reserved.
-//
-
-import Foundation
